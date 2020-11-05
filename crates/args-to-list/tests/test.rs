c4ab89114b15c99306a3fd9d58d66db529927473
khai96_ <hvksmr1996@gmail.com>
2020-11-05 18:09:41 +0700

    Add test

---
+++ crates/args-to-list/tests/test.rs
@@ -0,0 +1,31 @@
+use command_extra::CommandExtra;
+use pipe_trait::*;
+use std::process::Command;
+
+#[test]
+fn test() {
+    let output = env!("CARGO_BIN_EXE_args-to-list")
+        .pipe(Command::new)
+        .with_args(&["abc", "def", "ghi"])
+        .output()
+        .expect("get output");
+
+    let stdout = output
+        .stdout
+        .pipe(String::from_utf8)
+        .expect("decode stdout as UTF-8");
+
+    let stderr = output
+        .stderr
+        .pipe(String::from_utf8)
+        .expect("decode stderr as UTF-8");
+
+    assert_eq!(
+        (stdout, stderr, output.status.success()),
+        (
+            vec!["abc", "def", "ghi", ""].join("\n"),
+            "".to_string(),
+            true,
+        ),
+    )
+}
