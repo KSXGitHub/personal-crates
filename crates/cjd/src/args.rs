da37d560eb7ffaffcf4b764a2d1cfce6f5cc8c4c
khai96_ <hvksmr1996@gmail.com>
2022-06-10 10:26:57 +0700

    fmt: Fix

--- crates/cjd/src/args.rs
+++ crates/cjd/src/args.rs
@@ -8,7 +8,7 @@ pub struct Args {
         name = "input format",
         possible_values = &["json", "yaml", "toml", "json5"],
         parse(from_str = parse_format),
-)]
+    )]
     pub input_format: Format,
     #[structopt(
         name = "output format",
