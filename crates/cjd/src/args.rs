537e4dd439d16d2d1d2b680d00538aa3e663488e
khai96_ <hvksmr1996@gmail.com>
2022-08-10 21:29:10 +0700

    Replace `possible_values` with `value_enum`

--- crates/cjd/src/args.rs
+++ crates/cjd/src/args.rs
@@ -1,29 +1,10 @@
 use super::format::*;
 use clap::Parser;
-use std::hint::unreachable_unchecked;
 
 #[derive(Debug, Parser)]
 pub struct Args {
-    #[clap(
-        name = "input format",
-        possible_values = &["json", "yaml", "toml", "json5"],
-        parse(from_str = parse_format),
-    )]
+    #[clap(name = "input format", value_enum)]
     pub input_format: Format,
-    #[clap(
-        name = "output format",
-        possible_values = &["json", "yaml", "toml", "json5"],
-        parse(from_str = parse_format),
-    )]
+    #[clap(name = "output format", value_enum)]
     pub output_format: Format,
-}
-
-fn parse_format(argument: &str) -> Format {
-    match argument {
-        "json" => Json,
-        "yaml" => Yaml,
-        "toml" => Toml,
-        "json5" => Json5,
-        _ => unsafe { unreachable_unchecked() },
-    }
 }
