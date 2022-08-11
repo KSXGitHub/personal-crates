e64425204cb89ae46c145d1db09e3eb5ad9101aa
khai96_ <hvksmr1996@gmail.com>
2022-08-11 12:32:50 +0700

    Remove json5

--- crates/git-all-remotes/src/format.rs
+++ crates/git-all-remotes/src/format.rs
@@ -9,7 +9,6 @@ pub enum Format {
     Json,
     Yaml,
     Toml,
-    Json5,
 }
 
 /// Error caused by [`Format::serialize`]
@@ -19,11 +18,9 @@ pub enum SerializationError {
     Json(serde_json::Error),
     Yaml(serde_yaml::Error),
     Toml(toml::ser::Error),
-    Json5(json5::Error),
 }
 
 mod serialize {
-    pub use json5::to_string as json5;
     pub use serde_json::to_string_pretty as json;
     pub use serde_yaml::to_string as yaml;
     pub use toml::to_string_pretty as toml;
@@ -35,7 +32,6 @@ impl Format {
             Format::Json => serialize::json(data)?,
             Format::Yaml => serialize::yaml(data)?,
             Format::Toml => serialize::toml(data)?,
-            Format::Json5 => serialize::json5(data)?,
         })
     }
 }
