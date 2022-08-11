2074cde83f5c4a84fc94d9c0e2ad9b44895e0fdb
khai96_ <hvksmr1996@gmail.com>
2022-08-11 10:29:06 +0700

    feat: Avoid panic on invalid UTF-8

--- crates/git-all-remotes/src/main.rs
+++ crates/git-all-remotes/src/main.rs
@@ -5,8 +5,10 @@ use display_result::DisplayResult;
 use error::Error;
 use git2::Repository;
 use nu_ansi_term::Style;
+use os_display::Quotable;
+use os_str_bytes::OsStrBytes;
 use pipe_trait::*;
-use std::env::current_dir;
+use std::{env::current_dir, ffi::OsStr};
 
 fn app() -> Result<(), Error> {
     let repo = current_dir()
@@ -20,8 +22,11 @@ fn app() -> Result<(), Error> {
 
     for name in remotes.iter().flatten() {
         let remote = repo.find_remote(name).map_err(Error::Git)?;
-        let url = remote.url().expect("get url from remote");
-        println!("{}: {}", name_style.paint(name), url);
+        let url = remote
+            .url_bytes()
+            .pipe(OsStr::from_raw_bytes)
+            .map_err(Error::OsStrBytes)?;
+        println!("{}: {}", name_style.paint(name), url.maybe_quote());
     }
 
     Ok(())
